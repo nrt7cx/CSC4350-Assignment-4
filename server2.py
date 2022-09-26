1289f996d4264f860bd459c537566f95caebf8fe
nrt7cx <113043415+nrt7cx@users.noreply.github.com>
2022-09-26 11:50:58 -0400

    Add files via upload

--- server2.py
+++ server2.py
@@ -7,17 +7,22 @@ parser.add_argument("-p","--port", help="Sets the port for the server", type=int
 args = parser.parse_args()
 
 socket = socket(AF_INET, SOCK_STREAM)
+html_file = open("index.html", "a")
 #print("Please enter a specific page: ")
 #website= input()
-socket.connect(("www.imdb.com", args.port))
+socket.connect(("www.google.com", args.port))
 request_Part1 = "GET / HTTP/1.1\r\nHost:"
 print("Please enter a specific page: ")
 website= input()
 request = request_Part1 + website + "\r\n\r\n" 
 socket.send(request.encode())
+html_file.write(request)
+html_file.close
 
 response = socket.recv(10000)
-#if response[0:12].decode() == "HTTP/1.1 200":   
-    #print("HTTP 200 OK") 
-print(response.decode())
+if response[0:12].decode() == "HTTP/1.1 200":
+    html_file = open("index.html", "r")
+    print(html_file.read())
+    print(response.decode())
 socket.close
+html_file.close
