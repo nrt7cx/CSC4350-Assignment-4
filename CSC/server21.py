3550fa652ee821f3fc32b2c562896777d08fda06
nrt7cx <113043415+nrt7cx@users.noreply.github.com>
2022-09-27 22:56:50 -0400

    Add files via upload

--- CSC/server21.py
+++ CSC/server21.py
@@ -1,9 +1,9 @@
+from posixpath import split
 from socket import *
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p","--port", help="Sets the port for the server", type=int)
-#parser.add_argument("-h","--help", help="Prints out message on how to run the server", type=str)
 args = parser.parse_args()
 
 serverPort = args.port
@@ -15,21 +15,35 @@ print ("The server is ready to receive.")
 while True:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024).decode()
-    print(sentence)
+    splitSentence = sentence.split()
+    print(splitSentence)
+
+    if splitSentence[0] == "GET":
+
+        if splitSentence[1] == "/":
+                f = open('index.html', 'r') 
+                file_contents = f.read()
+                connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" + file_contents.encode() + b"\r\n\r\n")
+                connectionSocket.close()
     
-    if sentence[0:14] == "GET / HTTP/1.1":
-            f = open('index.html', 'r') 
-            file_contents = f.read()
-            connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" + file_contents.encode())
-            connectionSocket.close()
+        elif splitSentence[1] == "/index.html":
+                f = open('index.html', 'r') 
+                file_contents = f.read()
+                connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" + file_contents.encode() + b"\r\n\r\n")
+                connectionSocket.close()
         
-    if sentence[0:22] == "GET /epic.txt HTTP/1.1":
-            f = open('epic.txt', 'r')
-            file_contents = f.read()
-            connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" + file_contents.encode() + b"\r\n\r\n")
-              
+        elif splitSentence[1] == "/epic.txt":
+                r = open('epic.txt', 'r')
+                file_contents1 = r.read()
+                connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" + file_contents1.encode() + b"\r\n\r\n")
+                connectionSocket.close()
+        
+        else:
+                modifiedSentence = "HTTP/1.1 404 Not Found\r\n\r\n"
+                connectionSocket.send(modifiedSentence.encode())
+                connectionSocket.close()
+        
     else:
         modifiedSentence = "HTTP/1.1 404 Not Found\r\n\r\n"
         connectionSocket.send(modifiedSentence.encode())
-    
-    connectionSocket.close()
+        connectionSocket.close()
